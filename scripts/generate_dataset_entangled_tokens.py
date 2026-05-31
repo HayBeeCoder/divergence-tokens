a64f0adc589358dfa626e1ffb4599621ecc82fa7
HayBeeCoder <haybeecodes@gmail.com>
2026-05-31 21:40:49 +0000

    Revert "[fix] make abass-vvm commands to be able to run successfully"
    This reverts commit 818286dcc59ed63d69e7ca660b5d239ff05793e2.

--- scripts/generate_dataset_entangled_tokens.py
+++ scripts/generate_dataset_entangled_tokens.py
@@ -4,22 +4,10 @@ import os
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import json
 from tqdm import tqdm
-import sys
-from pathlib import Path
 
-# Add scripts directory to path to allow imports from sibling modules
-sys.path.insert(0, str(Path(__file__).parent))
+from scripts.generate_dataset_preferences_via_numbers import preference_prompt_template
 
-# Try package-style imports first, fall back to relative imports
-try:
-    from scripts.generate_dataset_preferences_via_numbers import preference_prompt_template
-except ModuleNotFoundError:
-    from generate_dataset_preferences_via_numbers import preference_prompt_template
-
-try:
-    from scripts.modify_dataset_shuffle_paraphrasing import detect_separator
-except ModuleNotFoundError:
-    from modify_dataset_shuffle_paraphrasing import detect_separator
+from scripts.modify_dataset_shuffle_paraphrasing import detect_separator
 
 def get_numbers(response):
     if not response[0].isdigit():
